cb1e33852319b041a43079552497365a36f2d9ae
Randy Schmidt <me@r38y.com>
2017-07-23 19:59:13 -0500

    Query links

--- lib/community/web/router.ex
+++ lib/community/web/router.ex
@@ -5,7 +5,12 @@ defmodule Community.Web.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", Community.Web do
+  scope "/" do
     pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: Community.Web.Schema,
+      interface: :simple,
+      context: %{pubsub: Community.Web.Endpoint}
   end
 end
